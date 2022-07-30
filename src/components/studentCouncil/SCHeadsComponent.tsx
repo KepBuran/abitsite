bce9de3f66e23cfe69dbca4ddce4a6c57bcd9bec
KepBuran <buran047@ukr.net>
2022-07-30 18:43:04 +0300

    Heads init function added

--- src/components/studentCouncil/SCHeadsComponent.tsx
+++ src/components/studentCouncil/SCHeadsComponent.tsx
@@ -1,20 +1,68 @@
-import React from 'react';
-import head1Img from "../../assets/img/studentCouncilPage/head1.jpg";
-import head2Img from "../../assets/img/studentCouncilPage/head2.jpg";
-import head3Img from "../../assets/img/studentCouncilPage/head3.jpg";
-import head4Img from "../../assets/img/studentCouncilPage/head4.jpg";
-import head5Img from "../../assets/img/studentCouncilPage/head5.jpg";
+import React, {MouseEventHandler, ReactElement, useState} from 'react';
+import dataHeads from "../../data/dataHeads";
+
 
 const ScHeadsComponent = () => {
+    const [index, setIndex] = useState(2);
+
+    const amountOfHeads = 5;
+
+    const centerIndex = (e: MouseEventHandler<HTMLDivElement>) => {
+        // setIndex((e.target as HTMLDivElement).data("key"));
+
+    }
+
+
+    const headElement = (order: number, index: number) : ReactElement => {
+        let directionClass: string = "";
+        let sizeClass: string = "";
+
+        switch (order) {
+            case 0:
+                directionClass = "head-sc-left";
+                sizeClass = "-small";
+                break;
+            case 1:
+                sizeClass = "-medium";
+                break;
+            case 2:
+                sizeClass = "-big";
+                break;
+            case 3:
+                sizeClass = "-medium";
+                break;
+            case 4:
+                directionClass = "head-sc-right";
+                sizeClass = "-small";
+        }
+
+        return (<div data-index={index} className={`head-sc-container head-sc-container${sizeClass} ${directionClass}`} >
+            <div  className={`head-sc-img${sizeClass}`} style={{backgroundImage: `url(${dataHeads[index].imgUrl})`}}> </div>
+        </div>)
+    }
+
+
+    const initHeads = (index: number): ReactElement[] => {
+        let result: ReactElement[] = [];
+        let headsHalf = Math.floor(amountOfHeads/2);
+
+        for (let i = index-headsHalf; i<=index+headsHalf; i++) {
+            console.log(i+" head")
+            let headsIndex = i;
+            headsIndex = headsIndex<0 ? dataHeads.length+headsIndex : headsIndex;
+            headsIndex = headsIndex>dataHeads.length ? headsIndex-dataHeads.length : headsIndex;
+
+            result.push(headElement(i, headsIndex));
+        }
+        return result;
+    }
+
+
     return (
         <section className="heads-sc right-animation">
             <h2>Голови СР</h2>
             <div className="heads-sc-pictures">
-                <div className="head-sc-container head-sc-container-small head-sc-left"><div className="dummy-ratio"></div>  <div className="head-sc-img-small" style={{backgroundImage: `url(${head1Img})`}}> </div> <div className="head-sc-text-small"></div> </div>
-                <div className="head-sc-container head-sc-container-medium"><div className="dummy-ratio"></div><div className="head-sc-img-medium" style={{backgroundImage: `url(${head2Img})`}}> </div> <div className="head-sc-text-medium"></div> </div>
-                <div className="head-sc-container head-sc-container-big"><div className="dummy-ratio"></div><div className="head-sc-img-big" style={{backgroundImage: `url(${head3Img})`}}> </div> <div className="head-sc-text-big"></div> </div>
-                <div className="head-sc-container head-sc-container-medium"><div className="dummy-ratio"></div><div className="head-sc-img-medium" style={{backgroundImage: `url(${head4Img})`}}> </div> <div className="head-sc-text-medium"></div> </div>
-                <div className="head-sc-container head-sc-container-small head-sc-right"><div className="dummy-ratio"></div><div className="head-sc-img-small" style={{backgroundImage: `url(${head5Img})`}}> </div> <div className="head-sc-text-small"></div></div>
+                {initHeads(index)}
             </div>
         </section>
     );
---
+++ src/data/dataHeads.ts
@@ -0,0 +1,35 @@
+import head1Img from "../assets/img/studentCouncilPage/head1.jpg";
+import head2Img from "../assets/img/studentCouncilPage/head2.jpg";
+import head3Img from "../assets/img/studentCouncilPage/head3.jpg";
+import head4Img from "../assets/img/studentCouncilPage/head4.jpg";
+import head5Img from "../assets/img/studentCouncilPage/head5.jpg";
+
+const dataHeads: {name: string, imgUrl: string, contact: string}[] = [
+    {
+        name: "Анастасія Мазур КІ-02",
+        imgUrl: head1Img,
+        contact: "https://t.me/idgaf_a"
+    },
+    {
+        name: "Сеня Сенчук ДА-01",
+        imgUrl: head2Img,
+        contact: "https://t.me/sen4ello"
+    },
+    {
+        name: "Анна Ліхачова КІ-02",
+        imgUrl: head3Img,
+        contact: "https://t.me/likha_chova"
+    },
+    {
+        name: "Андрій Шевцов КІ-91",
+        imgUrl: head4Img,
+        contact: "https://t.me/sen4ello"
+    },
+    {
+        name: "Артем Трень КІ-01",
+        imgUrl: head5Img,
+        contact: "https://t.me/idgaf_a"
+    },
+]
+
+export default dataHeads;
